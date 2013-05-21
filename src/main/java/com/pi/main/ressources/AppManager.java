ad40eaca8cc671c39adbd26688f85f4c3a22d0a0
Olivier Van Goethem <ov109@ic.ac.uk>
2013-05-21 16:16:14 +0100

    Further scaffolding for dynamically loading apps

--- src/main/java/com/pi/main/ressources/AppManager.java
+++ src/main/java/com/pi/main/ressources/AppManager.java
@@ -12,28 +12,24 @@ public class AppManager {
 	
 	private static String selectedURL;
 	private static ArrayList<App> appList;
-	private static ClassLoader classLoader;
+	private static ClassLoader loader;
+	private static final String appDir = "file:///home/pi/FYP/apps/";
 	
 	@SuppressWarnings("serial")
 	public AppManager() {
 		appList = new ArrayList<App>();
 		selectedURL = null;
-		try {
-			classLoader = new URLClassLoader(new URL[] {new URL("file:///home/pi/FYP/apache-tomcat-7.0.35/webapps/ROOT/WEB-INF/classes/")}, ConnectedApp.class.getClassLoader());
-		} catch (MalformedURLException e1) {
-			System.err.println("Unable to initiate app loader. Check that app folder exists!");
-			System.exit(1);
-		}
 		
 		//TODO Implement a proper add app function
 		try {
+			loader = new URLClassLoader(new URL[] {new URL(appDir + "lights-app-1.0.0.jar")}, ConnectedApp.class.getClassLoader());
 			addApp(new App.Builder()
 				.name("Lights")
 				.pageName("Let there be lights!")
 				.URL("lights")
 				.description("The 'Lights' application allows you to remotely monitor the energy consumption of a power socket. You can also turn it on or off!")
 				.icon("icon-lightbulb")
-				.app((ConnectedApp) classLoader.loadClass("com.pi.main.apps.LightsApp").newInstance())
+				.app((ConnectedApp) loader.loadClass("com.pi.LightsApp").newInstance())
 				.methodsAvailable(new HashMap<String,String>() {
 					{
 						put("On", "turnOn");
@@ -41,40 +37,40 @@ public class AppManager {
 					}
 				})
 				.build());
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		try {
+			loader = new URLClassLoader(new URL[] {new URL(appDir + "media-app-1.0.0.jar")}, ConnectedApp.class.getClassLoader());
 			addApp(new App.Builder()
 				.name("Media")
 				.pageName("Care to listen to some music?")
 				.URL("media")
 				.description("The 'Media' application allows you to listen to music or watch videos anywhere in your house!")
 				.icon("icon-music")
-				.app((ConnectedApp) classLoader.loadClass("com.pi.main.apps.MediaApp").newInstance())
+				.app((ConnectedApp) loader.loadClass("com.pi.MediaApp").newInstance())
 				.methodsAvailable(new HashMap<String,String>())
 				.build());
-		} catch (InstantiationException | IllegalAccessException
-				| ClassNotFoundException e) {
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		// DUMMY APPS FOR TESTING PURPOSES
 		try {
+			loader = new URLClassLoader(new URL[] {new URL("file:///home/pi/FYP/apache-tomcat-7.0.35/webapps/ROOT/WEB-INF/classes/")}, ConnectedApp.class.getClassLoader());
 			addApp(new App.Builder()
 				.name("Dummy")
 				.pageName("Danger: test zone")
 				.URL("dummy")
 				.description("This is where I test features before moving them into production.")
 				.icon("icon-circle-blank")
-				.app((ConnectedApp) classLoader.loadClass("com.pi.main.apps.DummyApp").newInstance())
+				.app((ConnectedApp) loader.loadClass("com.pi.main.apps.DummyApp").newInstance())
 				.methodsAvailable(new HashMap<String,String>())
 				.build());
-		} catch (InstantiationException | IllegalAccessException
-				| ClassNotFoundException e) {
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
