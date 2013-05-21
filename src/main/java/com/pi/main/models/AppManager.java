812ac2c4352920032e95706245b278ceff5ba697
Olivier Van Goethem <ov109@ic.ac.uk>
2013-05-21 17:53:16 +0100

    appLoader method provides generic way of adding apps to system

--- src/main/java/com/pi/main/models/AppManager.java
+++ src/main/java/com/pi/main/models/AppManager.java
@@ -1,6 +1,5 @@
 package com.pi.main.models;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
@@ -20,35 +19,9 @@ public class AppManager {
 		
 		//TODO Implement a proper add app function
 		try {
-			classLoader = new URLClassLoader(new URL[] {new URL(appDir + "lights-app-1.0.0.jar")}, ConnectedApp.class.getClassLoader());
-			appLoader.loadAppDetails(new URL(appDir + "lights.xml"));
-			addApp(new App.Builder()
-				.name(appLoader.getName())
-				.pageName(appLoader.getPageName())
-				.URL(appLoader.getUrl())
-				.description(appLoader.getDescription())
-				.icon(appLoader.getIcon())
-				.app((ConnectedApp) classLoader.loadClass(appLoader.getClassPackage()).newInstance())
-				.methodsAvailable(appLoader.getMethods())
-				.build());
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			classLoader = new URLClassLoader(new URL[] {new URL(appDir + "media-app-1.0.0.jar")}, ConnectedApp.class.getClassLoader());
-			appLoader.loadAppDetails(new URL(appDir + "media.xml"));
-			addApp(new App.Builder()
-				.name(appLoader.getName())
-				.pageName(appLoader.getPageName())
-				.URL(appLoader.getUrl())
-				.description(appLoader.getDescription())
-				.icon(appLoader.getIcon())
-				.app((ConnectedApp) classLoader.loadClass(appLoader.getClassPackage()).newInstance())
-				.methodsAvailable(appLoader.getMethods())
-				.build());
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | MalformedURLException e) {
+			loadApp("lights.xml");
+			loadApp("media.xml");
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -60,6 +33,20 @@ public class AppManager {
 	
 	public void setAppList(ArrayList<App> appList) {
 		AppManager.appList = appList;
+	}
+	
+	public void loadApp(String xmlFile) throws Exception {
+		appLoader.loadAppDetails(new URL(appDir + xmlFile));
+		classLoader = new URLClassLoader(new URL[] {new URL(appDir + appLoader.getLocation())}, ConnectedApp.class.getClassLoader());
+		addApp(new App.Builder()
+			.name(appLoader.getName())
+			.pageName(appLoader.getPageName())
+			.URL(appLoader.getUrl())
+			.description(appLoader.getDescription())
+			.icon(appLoader.getIcon())
+			.app((ConnectedApp) classLoader.loadClass(appLoader.getClassPackage()).newInstance())
+			.methodsAvailable(appLoader.getMethods())
+			.build());
 	}
 	
 	public void addApp(App app) {
